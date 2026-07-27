7ddfb6cd2c4f11df2784aa00e7a5a7cb26620b24
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-07-28 00:34:13 +0530

    Disable reactStrictMode in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   typescript: {
-    // TypeScript की छोटी-मोटी टाइप चेकिंग एरर्स से Vercel बिल्ड नहीं रुकेगा
     ignoreBuildErrors: true,
   },
   eslint: {
-    // ESLint वार्निंग्स की वजह से बिल्ड फेल नहीं होगा
     ignoreDuringBuilds: true,
   },
 }
